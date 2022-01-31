c4c9a02cefb190d712626d4f89a4bd41e41c4995
Aditya Subramanian <aditarun2008@gmail.com>
2022-01-31 05:39:28 -0500

    Removed vowel pair implementation

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 use std::collections::HashSet;
 use std::str::FromStr;
+
 mod pre_compute;
 mod setup;
 
 fn main() {
     let mut yellow_chars: HashSet<char> = HashSet::new();
     let mut grey_chars: HashSet<char> = HashSet::new();
-    yellow_chars.insert('s');
-    yellow_chars.insert('l');
-    yellow_chars.insert('e');
+
     grey_chars.insert('b');
     grey_chars.insert('r');
-    grey_chars.insert('d');
-    grey_chars.insert('c');
-    grey_chars.insert('h');
-    grey_chars.insert('g');
-    grey_chars.insert('u');
-    grey_chars.insert('z');
-    grey_chars.insert('k');
-    grey_chars.insert('a');
     grey_chars.insert('o');
+    grey_chars.insert('d');
+    grey_chars.insert('s');
+
+    grey_chars.insert('c');
+    yellow_chars.insert('h');
+    grey_chars.insert('a');
+    yellow_chars.insert('l');
+    grey_chars.insert('k');
+
     grey_chars.insert('v');
-    grey_chars.insert('i');
+    yellow_chars.insert('i');
     grey_chars.insert('n');
+    grey_chars.insert('e');
     grey_chars.insert('w');
-    grey_chars.insert('m');
-    grey_chars.insert('p');
 
     let words: Vec<String> = setup::get_all_five_letter_words();
     let words_data_struct: pre_compute::WorDS = pre_compute::compute(words);
@@ -38,168 +37,54 @@ fn main() {
     }
     let good_guesses: HashSet<String> =
         get_good_guess(&yellow_chars, &grey_chars, &words_data_struct);
+    print!("guess:: {}\n", good_guesses.len());
     for guess in good_guesses {
         print!("guess:: {}\n", guess);
     }
 }
+
 fn hash_to_ref(h: &HashSet<String>) -> HashSet<&str> {
     h.iter().map(|s| s.as_str()).collect()
 }
+
 fn hash_to_string(h: HashSet<&str>) -> HashSet<String> {
     h.iter().map(|s| s.to_string()).collect()
 }
+
 fn get_word_possibilities(
     y_chars: &HashSet<char>,
     g_chars: &HashSet<char>,
     words_data_struct: &pre_compute::WorDS,
 ) -> HashSet<String> {
     let mut result: Option<HashSet<&str>> = None;
-    let mut a = false;
-    let mut e = false;
-    let mut i = false;
-    let mut o = false;
-    let mut u = false;
     for ch in y_chars {
-        match ch {
-            'a' => {
-                a = true;
-            }
-            'e' => {
-                e = true;
-            }
-            'i' => {
-                i = true;
-            }
-            'o' => {
-                o = true;
-            }
-            'u' => {
-                u = true;
-            }
-            consonant_ch => {
-                let char_consonant =
-                    pre_compute::Consonant::from_str(&consonant_ch.to_string()).unwrap();
-                let char_words: HashSet<&str> = hash_to_ref(
-                    words_data_struct
-                        .consonant_map
-                        .get(&char_consonant)
-                        .clone()
-                        .unwrap(),
-                );
-                result = match result {
-                    None => Some(char_words),
-                    Some(r) => Some(r.intersection(&char_words).cloned().collect()),
-                };
-            }
-        }
-    }
-    let y_vowel_pair: HashSet<pre_compute::VowelPair> = get_vowel_pair_set(a, e, i, o, u);
-    for vp in y_vowel_pair {
-        let vowel_pair_words: HashSet<&str> =
-            hash_to_ref(words_data_struct.vowel_map.get(&vp).unwrap());
+        let char_consonant = pre_compute::Consonant::from_str(&ch.to_string()).unwrap();
+        let char_words: HashSet<&str> = hash_to_ref(
+            words_data_struct
+                .consonant_map
+                .get(&char_consonant)
+                .clone()
+                .unwrap(),
+        );
         result = match result {
-            None => Some(vowel_pair_words),
-            Some(r) => Some(vowel_pair_words.difference(&r).cloned().collect()),
-        }
+            None => Some(char_words),
+            Some(r) => Some(r.intersection(&char_words).cloned().collect()),
+        };
     }
-    a = false;
-    e = false;
-    i = false;
-    o = false;
-    u = false;
     for ch in g_chars {
-        match ch {
-            'a' => {
-                a = true;
-            }
-            'e' => {
-                e = true;
-            }
-            'i' => {
-                i = true;
-            }
-            'o' => {
-                o = true;
-            }
-            'u' => {
-                u = true;
-            }
-            consonant_ch => {
-                let char_consonant =
-                    pre_compute::Consonant::from_str(&consonant_ch.to_string()).unwrap();
-                let char_words: HashSet<&str> = hash_to_ref(
-                    words_data_struct
-                        .consonant_map
-                        .get(&char_consonant)
-                        .unwrap(),
-                );
-                result = match result {
-                    None => None,
-                    Some(r) => Some(r.difference(&char_words).cloned().collect()),
-                };
-            }
-        }
-    }
-    let g_vowel_pair: HashSet<pre_compute::VowelPair> = get_vowel_pair_set(a, e, i, o, u);
-    for vp in g_vowel_pair {
-        let vowel_pair_words: HashSet<&str> =
-            hash_to_ref(words_data_struct.vowel_map.get(&vp).unwrap());
+        let char_consonant = pre_compute::Consonant::from_str(&ch.to_string()).unwrap();
+        let char_words: HashSet<&str> = hash_to_ref(
+            words_data_struct
+                .consonant_map
+                .get(&char_consonant)
+                .unwrap(),
+        );
         result = match result {
             None => None,
-            Some(r) => Some(r.difference(&vowel_pair_words).cloned().collect()),
-        }
+            Some(r) => Some(r.difference(&char_words).cloned().collect()),
+        };
     }
     return hash_to_string(result.unwrap());
-}
-
-fn get_vowel_pair_set(
-    a: bool,
-    e: bool,
-    i: bool,
-    o: bool,
-    u: bool,
-) -> HashSet<pre_compute::VowelPair> {
-    let mut y_vowel_pair: HashSet<pre_compute::VowelPair> = HashSet::new();
-    if a {
-        if e {
-            y_vowel_pair.insert(pre_compute::VowelPair::AE);
-        }
-        if i {
-            y_vowel_pair.insert(pre_compute::VowelPair::AI);
-        }
-        if o {
-            y_vowel_pair.insert(pre_compute::VowelPair::AO);
-        }
-        if u {
-            y_vowel_pair.insert(pre_compute::VowelPair::AU);
-        }
-    }
-    if e {
-        if i {
-            y_vowel_pair.insert(pre_compute::VowelPair::EI);
-        }
-        if o {
-            y_vowel_pair.insert(pre_compute::VowelPair::EO);
-        }
-        if u {
-            y_vowel_pair.insert(pre_compute::VowelPair::EU);
-        }
-    }
-    if i {
-        if o {
-            y_vowel_pair.insert(pre_compute::VowelPair::IO);
-        }
-        if u {
-            y_vowel_pair.insert(pre_compute::VowelPair::IU);
-        }
-    }
-    if o {
-        if u {
-            y_vowel_pair.insert(pre_compute::VowelPair::OU);
-        }
-    }
-
-    return y_vowel_pair;
 }
 
 fn get_good_guess(
@@ -207,57 +92,34 @@ fn get_good_guess(
     grey_chars: &HashSet<char>,
     word_ds: &pre_compute::WorDS,
 ) -> HashSet<String> {
-    let chars: HashSet<char> = yellow_chars.union(&grey_chars).cloned().collect();
+    let chars: HashSet<char> = yellow_chars.union(grey_chars).cloned().collect();
     let mut result: Option<HashSet<&str>> = Some(hash_to_ref(
         word_ds
             .consonant_map
             .get(&pre_compute::Consonant::ALL)
             .unwrap(),
     ));
-    let mut a = false;
-    let mut e = false;
-    let mut i = false;
-    let mut o = false;
-    let mut u = false;
-    for ch in chars {
-        match ch {
-            'a' => {
-                a = true;
-            }
-            'e' => {
-                e = true;
-            }
-            'i' => {
-                i = true;
-            }
-            'o' => {
-                o = true;
-            }
-            'u' => {
-                u = true;
-            }
-            consonant_ch => {
-                let char_consonant =
-                    pre_compute::Consonant::from_str(&consonant_ch.to_string()).unwrap();
-                let char_words: HashSet<&str> =
-                    hash_to_ref(word_ds.consonant_map.get(&char_consonant).unwrap());
-                result = match result {
-                    None => None,
-                    Some(r) => Some(r.difference(&char_words).cloned().collect()),
-                };
-            }
-        }
+    match result {
+        None => print!("None"),
+        Some(ref r) => print!("{}\n", r.len()),
     }
-
-    let g_vowel_pair: HashSet<pre_compute::VowelPair> = get_vowel_pair_set(a, e, i, o, u);
-    for vp in g_vowel_pair {
-        let vowel_pair_words: HashSet<&str> = hash_to_ref(word_ds.vowel_map.get(&vp).unwrap());
+    for ch in chars {
+        match result {
+            None => print!("None"),
+            Some(ref r) => print!("{} removal {}\n", &ch, r.len()),
+        }
+        let char_consonant = pre_compute::Consonant::from_str(&ch.to_string()).unwrap();
+        let char_words: HashSet<&str> =
+            hash_to_ref(word_ds.consonant_map.get(&char_consonant).unwrap());
         result = match result {
             None => None,
-            Some(r) => Some(r.difference(&vowel_pair_words).cloned().collect()),
-        }
+            Some(r) => Some(r.difference(&char_words).cloned().collect()),
+        };
     }
-
+    match result {
+        None => print!("None"),
+        Some(ref r) => print!("now: {}\n", r.len()),
+    }
     match result {
         None => HashSet::new(),
         Some(r) => hash_to_string(r),
--- src/pre_compute.rs
+++ src/pre_compute.rs
@@ -5,51 +5,29 @@ use std::hash::Hash;
 use std::str::FromStr;
 
 #[derive(PartialEq, Eq, Hash)]
-pub enum VowelPair {
-    AA,
-    AE,
-    AI,
-    AO,
-    AU,
-    EE,
-    EI,
-    EO,
-    EU,
-    II,
-    IO,
-    IU,
-    OO,
-    OU,
-    UU,
-}
-
-enum Vowel {
-    A,
-    E,
-    I,
-    O,
-    U,
-}
-
-#[derive(PartialEq, Eq, Hash)]
 pub enum Consonant {
     ALL,
+    A,
     B,
     C,
     D,
+    E,
     F,
     G,
     H,
+    I,
     J,
     K,
     L,
     M,
     N,
+    O,
     P,
     Q,
     R,
     S,
     T,
+    U,
     V,
     W,
     X,
@@ -83,6 +61,11 @@ impl FromStr for Consonant {
             'x' => Ok(Consonant::X),
             'y' => Ok(Consonant::Y),
             'z' => Ok(Consonant::Z),
+            'a' => Ok(Consonant::A),
+            'e' => Ok(Consonant::E),
+            'i' => Ok(Consonant::I),
+            'o' => Ok(Consonant::O),
+            'u' => Ok(Consonant::U),
             _ => Err(()),
         }
     }
@@ -90,186 +73,32 @@ impl FromStr for Consonant {
 
 pub struct WorDS {
     pub consonant_map: HashMap<Consonant, HashSet<String>>,
-    pub vowel_map: HashMap<VowelPair, HashSet<String>>,
 }
 
 pub fn compute(words: Vec<String>) -> WorDS {
     let mut word_ds = WorDS {
-        vowel_map: HashMap::new(),
         consonant_map: HashMap::new(),
     };
     for word in &words {
         if word.len() != 5 {
             panic!("ye to cheating hai!");
         }
-        let mut a = false;
-        let mut e = false;
-        let mut i = false;
-        let mut o = false;
-        let mut u = false;
         for ch in word.chars() {
-            if ['a', 'e', 'i', 'o', 'u'].contains(&ch) {
-                let double_vowels: HashSet<VowelPair> = get_double_vowels(ch, a, e, i, o, u);
-                for vp in double_vowels {
-                    match word_ds.vowel_map.get_mut(&vp) {
-                        None => {
-                            let mut word_set: HashSet<String> = HashSet::new();
-                            word_set.insert(word.to_string());
-                            word_ds.vowel_map.insert(vp, word_set);
-                        }
-                        Some(word_set) => {
-                            word_set.insert(word.to_string());
-                        }
-                    }
+            let consonant: Consonant = Consonant::from_str(&ch.to_string()).unwrap();
+            match word_ds.consonant_map.get_mut(&consonant) {
+                None => {
+                    let mut word_set: HashSet<String> = HashSet::new();
+                    word_set.insert(word.to_string());
+                    word_ds.consonant_map.insert(consonant, word_set);
                 }
-            }
-            match ch {
-                'a' => {
-                    a = true;
+                Some(word_set) => {
+                    word_set.insert(word.to_string());
                 }
-                'e' => {
-                    e = true;
-                }
-                'i' => {
-                    i = true;
-                }
-                'o' => {
-                    o = true;
-                }
-                'u' => {
-                    u = true;
-                }
-                consonant_ch => {
-                    let consonant: Consonant =
-                        Consonant::from_str(&consonant_ch.to_string()).unwrap();
-                    match word_ds.consonant_map.get_mut(&consonant) {
-                        None => {
-                            let mut word_set: HashSet<String> = HashSet::new();
-                            word_set.insert(word.to_string());
-                            word_ds.consonant_map.insert(consonant, word_set);
-                        }
-                        Some(word_set) => {
-                            word_set.insert(word.to_string());
-                        }
-                    };
-                }
-            }
+            };
         }
     }
     word_ds
         .consonant_map
         .insert(Consonant::ALL, words.into_iter().collect());
     return word_ds;
-}
-
-fn get_double_vowels(
-    curr_char: char,
-    a: bool,
-    e: bool,
-    i: bool,
-    o: bool,
-    u: bool,
-) -> HashSet<VowelPair> {
-    let mut double_vowels: HashSet<VowelPair> = HashSet::new();
-    if a {
-        match curr_char {
-            'a' => {
-                double_vowels.insert(VowelPair::AA);
-            }
-            'e' => {
-                double_vowels.insert(VowelPair::AE);
-            }
-            'i' => {
-                double_vowels.insert(VowelPair::AI);
-            }
-            'o' => {
-                double_vowels.insert(VowelPair::AO);
-            }
-            'u' => {
-                double_vowels.insert(VowelPair::AU);
-            }
-            _other => panic!("ye to cheating hai"),
-        }
-    }
-    if e {
-        match curr_char {
-            'a' => {
-                double_vowels.insert(VowelPair::AE);
-            }
-            'e' => {
-                double_vowels.insert(VowelPair::EE);
-            }
-            'i' => {
-                double_vowels.insert(VowelPair::EI);
-            }
-            'o' => {
-                double_vowels.insert(VowelPair::EO);
-            }
-            'u' => {
-                double_vowels.insert(VowelPair::EU);
-            }
-            _other => panic!("ye to cheating hai"),
-        }
-    }
-    if i {
-        match curr_char {
-            'a' => {
-                double_vowels.insert(VowelPair::AI);
-            }
-            'e' => {
-                double_vowels.insert(VowelPair::EI);
-            }
-            'i' => {
-                double_vowels.insert(VowelPair::II);
-            }
-            'o' => {
-                double_vowels.insert(VowelPair::IO);
-            }
-            'u' => {
-                double_vowels.insert(VowelPair::IU);
-            }
-            _other => panic!("ye to cheating hai"),
-        }
-    }
-    if o {
-        match curr_char {
-            'a' => {
-                double_vowels.insert(VowelPair::AO);
-            }
-            'e' => {
-                double_vowels.insert(VowelPair::EO);
-            }
-            'i' => {
-                double_vowels.insert(VowelPair::IO);
-            }
-            'o' => {
-                double_vowels.insert(VowelPair::OO);
-            }
-            'u' => {
-                double_vowels.insert(VowelPair::OU);
-            }
-            _other => panic!("ye to cheating hai"),
-        }
-    }
-    if u {
-        match curr_char {
-            'a' => {
-                double_vowels.insert(VowelPair::AU);
-            }
-            'e' => {
-                double_vowels.insert(VowelPair::EU);
-            }
-            'i' => {
-                double_vowels.insert(VowelPair::IU);
-            }
-            'o' => {
-                double_vowels.insert(VowelPair::OU);
-            }
-            'u' => {
-                double_vowels.insert(VowelPair::UU);
-            }
-            _other => panic!("ye to cheating hai"),
-        }
-    }
-    return double_vowels;
 }
